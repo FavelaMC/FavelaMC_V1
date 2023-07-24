7a346482d9e54b5ec7315dbd06211f20b099b742
Reinaldy Oliveira <reinaldymc4@gmail.com>
2023-07-24 14:18:28 -0300

    Registering Inventory in the launcher

--- Engine/src/main/java/xyz/favelamc/engine/bukkit/BukkitMain.java
+++ Engine/src/main/java/xyz/favelamc/engine/bukkit/BukkitMain.java
@@ -1,6 +1,9 @@
 package xyz.favelamc.engine.bukkit;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import xyz.favelamc.engine.bukkit.inventorys.AccountInventory;
 
 public class BukkitMain extends JavaPlugin {
 
@@ -18,6 +21,17 @@ public class BukkitMain extends JavaPlugin {
             System.out.print("[Engine] -> Iniciado!");
         } catch (Exception exception) {
             System.out.print("[Engine] -> Ocorreu um erro ao inicializar: (" + exception + ")");
+        }
+    }
+
+    protected void enableInventorys() {
+        try {
+            PluginManager pluginManager = Bukkit.getPluginManager();
+            pluginManager.registerEvents(new AccountInventory(), this);
+
+            System.out.print("[Inventorys] -> Iniciado!");
+        } catch (Exception exception) {
+            System.out.print("[Inventorys] -> Ocorreu um erro ao inicializar: (" + exception + ")");
         }
     }
 
