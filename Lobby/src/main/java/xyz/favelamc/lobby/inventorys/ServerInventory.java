28d2d4d9902a7c4a10816c1652d193ec0545864c
Reinaldy Oliveira <reinaldymc4@gmail.com>
2023-07-24 14:47:55 -0300

    Created Inventory Server for view server's

---
+++ Lobby/src/main/java/xyz/favelamc/lobby/inventorys/ServerInventory.java
@@ -0,0 +1,40 @@
+package xyz.favelamc.lobby.inventorys;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import xyz.favelamc.engine.bukkit.BukkitLoader;
+import xyz.favelamc.engine.bukkit.api.ItemBuilder;
+
+public class ServerInventory implements Listener {
+
+    public static void inventoryServer(Player player) {
+        Inventory inventory = Bukkit.createInventory(player, 3 * 9, "§8Jogos");
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(BukkitLoader.getPlugin(), () -> {
+            inventory.setItem(13, new ItemBuilder().newItem(Material.DIAMOND_CHESTPLATE, "§aDuels"
+                    , new String[] {}, 1, (byte) 0));
+        }, 0, 20);
+        player.openInventory(inventory);
+    }
+
+    @EventHandler
+    public void inventoryClickEvent(InventoryClickEvent inventoryClickEvent) {
+        Player player = (Player) inventoryClickEvent.getWhoClicked();
+
+        if (inventoryClickEvent.getInventory().getTitle().equalsIgnoreCase("§8Jogos")
+                && (inventoryClickEvent.getCurrentItem() != null)
+                && (inventoryClickEvent.getCurrentItem().getTypeId() != 0)) {
+            inventoryClickEvent.setCancelled(true);
+
+            if (inventoryClickEvent.getCurrentItem().getType().equals(Material.DIAMOND_CHESTPLATE)) {
+                inventoryClickEvent.setCancelled(true);
+                player.sendMessage("§cAtualmente está indisponível!");
+            }
+        }
+    }
+}
