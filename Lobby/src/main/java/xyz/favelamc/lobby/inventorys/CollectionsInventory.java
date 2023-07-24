c643b7d93b08b15b58d74b54a33835d478566c2c
Reinaldy Oliveira <reinaldymc4@gmail.com>
2023-07-24 14:48:18 -0300

    Created Inventory Collections for view Collection's

---
+++ Lobby/src/main/java/xyz/favelamc/lobby/inventorys/CollectionsInventory.java
@@ -0,0 +1,39 @@
+package xyz.favelamc.lobby.inventorys;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import xyz.favelamc.engine.bukkit.BukkitLoader;
+import xyz.favelamc.engine.bukkit.api.ItemBuilder;
+
+public class CollectionsInventory implements Listener {
+
+    public static void inventoryCollections(Player player) {
+        Inventory inventory = Bukkit.createInventory(player, 3 * 9, "§8Colecionáveis");
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(BukkitLoader.getPlugin(), () -> {
+            inventory.setItem(13, new ItemBuilder().newItem(Material.BARRIER, "§c???"
+                    , new String[] {}, 1, (byte) 0));
+        }, 0, 20);
+        player.openInventory(inventory);
+    }
+
+    @EventHandler
+    public void inventoryClickEvent(InventoryClickEvent inventoryClickEvent) {
+        Player player = (Player) inventoryClickEvent.getWhoClicked();
+
+        if (inventoryClickEvent.getInventory().getTitle().equalsIgnoreCase("§8Colecionáveis")
+                && (inventoryClickEvent.getCurrentItem() != null)
+                && (inventoryClickEvent.getCurrentItem().getTypeId() != 0)) {
+            inventoryClickEvent.setCancelled(true);
+
+            if (inventoryClickEvent.getCurrentItem().getType().equals(Material.BARRIER)) {
+                inventoryClickEvent.setCancelled(true);
+            }
+        }
+    }
+}
