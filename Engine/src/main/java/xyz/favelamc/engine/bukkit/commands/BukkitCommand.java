3a1ab1c09f3964ee1454f836d86085b72f4f21a3
Reinaldy Oliveira <reinaldymc4@gmail.com>
2023-07-24 17:15:36 -0300

    BukkitCommand class for command's bukkit

---
+++ Engine/src/main/java/xyz/favelamc/engine/bukkit/commands/BukkitCommand.java
@@ -0,0 +1,24 @@
+package xyz.favelamc.engine.bukkit.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public abstract class BukkitCommand implements CommandExecutor {
+
+    public boolean isPlayer(CommandSender commandSender)
+    {
+        return (commandSender instanceof Player);
+    }
+
+    public Player getPlayerFromArgs(String player)
+    {
+        return Bukkit.getPlayer(player);
+    }
+
+    public Player getPlayerFromSender(CommandSender commandSender)
+    {
+        return (Player) commandSender;
+    }
+}
