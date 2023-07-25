bb16bd76c04e0a2459fb73c399e8fef263cb0e65
Reinaldy Oliveira <reinaldymc4@gmail.com>
2023-07-25 12:18:02 -0300

    (V2) system ranks and tags with general server

--- Engine/src/main/java/xyz/favelamc/engine/bukkit/account/ranks/Tag.java
+++ Engine/src/main/java/xyz/favelamc/engine/bukkit/account/ranks/Tag.java
@@ -2,33 +2,38 @@ package xyz.favelamc.engine.bukkit.account.ranks;
 
 public enum Tag {
 
-    DONO("§4", "§4§lDONO §4"),
+    DONO("§4", "Dono", "§4§lDONO §4"),
 
-    ADMIN("§c", "§c§lADMIN §c"),
-    MODPLUS("§5", "§5§lMOD+ §5"),
-    MOD("§5", "§5§lMOD §5"),
-    TRIAL("§5", "§5§lTRIAL §5"),
-    HELPER("§9", "§9§lHELPER §9"),
+    ADMIN("§c", "Admin" ,"§c§lADMIN §c"),
+    MODPLUS("§5", "Mod+" ,"§5§lMOD+ §5"),
+    MOD("§5", "Mod", "§5§lMOD §5"),
+    TRIAL("§5", "Trial", "§5§lTRIAL §5"),
+    HELPER("§9", "Helper", "§9§lHELPER §9"),
 
-    YOUTUBERPLUS("§3", "§3§lYT+ §3"),
-    YOUTUBER("§b", "§b§lYT §b"),
-    STREAMER("§5", "§5§lSTREAMER §5"),
+    YOUTUBERPLUS("§3", "YT+", "§3§lYT+ §3"),
+    YOUTUBER("§b", "YT", "§b§lYT §b"),
+    STREAMER("§5", "Streamer", "§5§lSTREAMER §5"),
 
-    BETA("§1", "§1§lBETA §1"),
-    GOD("§6", "§6§lGOD §6"),
-    VIP("§a", "§a§lVIP §a"),
+    BETA("§1", "Beta", "§1§lBETA §1"),
+    GOD("§6","God", "§6§lGOD §6"),
+    VIP("§a","Vip", "§a§lVIP §a"),
 
-    MEMBRO("§7", "§7* ");
+    MEMBRO("§7","Membro", "§7* ");
 
-    private String color, preffix;
+    private final String color, name, preffix;
 
-    Tag(String color, String preffix) {
+    Tag(String color, String name, String preffix) {
         this.color = color;
+        this.name = name;
         this.preffix = preffix;
     }
 
     public String getColor() {
         return color;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public String getPreffix() {
