75ae9b73d68e313584197e3d49a6e220073b6f7b
Reinaldy Oliveira <reinaldymc4@gmail.com>
2023-07-29 14:28:36 -0300

    Remove methodo loginEvent

--- Engine/src/main/java/xyz/favelamc/engine/bungeecord/listeners/BungeeCordListener.java
+++ Engine/src/main/java/xyz/favelamc/engine/bungeecord/listeners/BungeeCordListener.java
@@ -1,7 +1,6 @@
 package xyz.favelamc.engine.bungeecord.listeners;
 
 import net.md_5.bungee.api.ServerPing;
-import net.md_5.bungee.api.event.LoginEvent;
 import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -30,16 +29,6 @@ public class BungeeCordListener implements Listener {
                 serverPing.setDescription("          §b§lFAVELA §8» §a" + new FavelaMC().getStore() + "" +
                         "\n            §e§lADQUIRA O RANK: §1§lBETA");
                 break;
-        }
-    }
-
-    @EventHandler
-    public void loginEvent(LoginEvent loginEvent) {
-        String name = loginEvent.getConnection().getName();
-
-        if (name.length() > 16) {
-            loginEvent.setCancelled(true);
-            loginEvent.setCancelReason("§cOcorreu um erro ao conectar!\nseu nick possui mais de 16 caracters.");
         }
     }
 }
