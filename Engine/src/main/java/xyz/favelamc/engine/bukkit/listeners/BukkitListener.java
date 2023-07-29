ac43e7ba48d2c496de53c402a22f4eb68f3bc338
Reinaldy Oliveira <reinaldymc4@gmail.com>
2023-07-29 15:23:43 -0300

    Remove methodo PlayerLoginEvent

--- Engine/src/main/java/xyz/favelamc/engine/bukkit/listeners/BukkitListener.java
+++ Engine/src/main/java/xyz/favelamc/engine/bukkit/listeners/BukkitListener.java
@@ -2,21 +2,10 @@ package xyz.favelamc.engine.bukkit.listeners;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class BukkitListener implements Listener {
-
-    @EventHandler
-    public void asyncPlayerPreLoginEvent(AsyncPlayerPreLoginEvent asyncPlayerPreLoginEvent) {
-        String name = asyncPlayerPreLoginEvent.getName();
-
-        if (name.length() > 16) {
-            asyncPlayerPreLoginEvent.disallow(AsyncPlayerPreLoginEvent.Result.KICK_WHITELIST
-                    , "\n§cOcorreu um erro ao conectar!\nseu nick possui mais de 16 caracters.");
-        }
-    }
 
     @EventHandler
     public void playerJoinEvent(PlayerJoinEvent playerJoinEvent) {
