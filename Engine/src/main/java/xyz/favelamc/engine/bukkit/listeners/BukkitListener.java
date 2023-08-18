10eda626d78ef574525c71630e6eeaa4ab704673
Reinaldy Oliveira <reinaldymc4@gmail.com>
2023-08-18 17:13:06 -0300

    Update class BukkitListener

--- Engine/src/main/java/xyz/favelamc/engine/bukkit/listeners/BukkitListener.java
+++ Engine/src/main/java/xyz/favelamc/engine/bukkit/listeners/BukkitListener.java
@@ -1,15 +1,51 @@
 package xyz.favelamc.engine.bukkit.listeners;
 
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerAchievementAwardedEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.weather.WeatherChangeEvent;
 
 public class BukkitListener implements Listener {
 
     @EventHandler
+    public void foodLevelChangeEvent(FoodLevelChangeEvent foodLevelChangeEvent) {
+        foodLevelChangeEvent.setCancelled(true);
+    }
+
+    @EventHandler
+    public void playerAchievementAwardedEvent(PlayerAchievementAwardedEvent playerAchievementAwardedEvent) {
+        playerAchievementAwardedEvent.setCancelled(true);
+    }
+
+    @EventHandler
+    public void weatherChangeEvent(WeatherChangeEvent weatherChangeEvent) {
+        weatherChangeEvent.setCancelled(true);
+    }
+
+    @EventHandler
     public void playerJoinEvent(PlayerJoinEvent playerJoinEvent) {
         playerJoinEvent.setJoinMessage(null);
+    }
+
+    @EventHandler
+    public void asyncPlayerChatEvent(AsyncPlayerChatEvent asyncPlayerChatEvent) {
+        Player player = asyncPlayerChatEvent.getPlayer();
+
+        if (player.hasPermission("")) {
+            asyncPlayerChatEvent.setFormat(player.getDisplayName() + "§5 » §7" + asyncPlayerChatEvent.getMessage().replace("%", "%%").replace("&", "§"));
+        } else {
+            asyncPlayerChatEvent.setFormat(player.getDisplayName() + "§5 » §7" + asyncPlayerChatEvent.getMessage().replace("%", "%%"));
+        }
+
+        World world = asyncPlayerChatEvent.getPlayer().getWorld();
+
+        asyncPlayerChatEvent.getRecipients().retainAll(world.getPlayers());
     }
 
     @EventHandler
